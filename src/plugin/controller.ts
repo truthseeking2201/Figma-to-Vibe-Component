cea592a40614b00f7ff1492c867b16f6faf9486b
truthseeking2201 <112937871+truthseeking2201@users.noreply.github.com>
2025-05-22 16:11:31 +0700

    feat: capture more node properties

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,7 +1,7 @@
 figma.showUI(__html__, { width: 400, height: 300 });
 
 function extractProperties(node: SceneNode) {
-  return {
+  const props: Record<string, any> = {
     id: node.id,
     name: node.name,
     type: node.type,
@@ -9,7 +9,66 @@ function extractProperties(node: SceneNode) {
     y: node.y,
     width: node.width,
     height: node.height,
+    rotation: node.rotation,
+    opacity: node.opacity,
+    visible: node.visible,
   };
+
+  if ('fills' in node && node.fills !== figma.mixed) {
+    props.fills = node.fills;
+  }
+
+  if ('strokes' in node && node.strokes !== figma.mixed) {
+    props.strokes = node.strokes;
+    props.strokeWeight = (node as GeometryMixin & SceneNode).strokeWeight;
+  }
+
+  if ('cornerRadius' in node) {
+    const cornerNode = node as GeometryMixin & CornerMixin;
+    if (typeof cornerNode.cornerRadius === 'number') {
+      props.cornerRadius = cornerNode.cornerRadius;
+    } else {
+      props.cornerRadius = {
+        topLeft: cornerNode.topLeftRadius,
+        topRight: cornerNode.topRightRadius,
+        bottomLeft: cornerNode.bottomLeftRadius,
+        bottomRight: cornerNode.bottomRightRadius,
+      };
+    }
+  }
+
+  if ('effects' in node) {
+    props.effects = node.effects;
+  }
+
+  if ('constraints' in node) {
+    props.constraints = node.constraints;
+  }
+
+  if ('layoutMode' in node) {
+    const layoutNode = node as FrameNode;
+    props.layoutMode = layoutNode.layoutMode;
+    props.primaryAxisSizingMode = layoutNode.primaryAxisSizingMode;
+    props.counterAxisSizingMode = layoutNode.counterAxisSizingMode;
+    props.itemSpacing = layoutNode.itemSpacing;
+    props.paddingLeft = layoutNode.paddingLeft;
+    props.paddingRight = layoutNode.paddingRight;
+    props.paddingTop = layoutNode.paddingTop;
+    props.paddingBottom = layoutNode.paddingBottom;
+  }
+
+  if (node.type === 'TEXT') {
+    const textNode = node as TextNode;
+    props.characters = textNode.characters;
+    props.fontName = textNode.fontName;
+    props.fontSize = textNode.fontSize;
+    props.textAlignHorizontal = textNode.textAlignHorizontal;
+    props.textAlignVertical = textNode.textAlignVertical;
+    props.lineHeight = textNode.lineHeight;
+    props.letterSpacing = textNode.letterSpacing;
+  }
+
+  return props;
 }
 
 function collectNodeProperties(node: SceneNode): any {
